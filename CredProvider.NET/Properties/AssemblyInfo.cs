a777a654c67637251dfefbe5121d9ef6c844202c
Fredrik Orderud <fredrik.orderud@ge.com>
2022-03-28 21:45:11 +0200

    Remove unecessary TypeLib GUIDs.

--- CredProvider.NET/Properties/AssemblyInfo.cs
+++ CredProvider.NET/Properties/AssemblyInfo.cs
@@ -19,9 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("edd905a1-1c64-4db4-8e21-a66f70903fbd")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
