f4d750213e0c0262e3b733cf0ced50ea80b21b8c
Fredrik Orderud <fredrik.orderud@ge.com>
2022-05-03 12:44:10 +0200

    Change to unique CLSID
    Change to a unique GUID that is not shared with CredProvider.NET.

--- ReversePassword/Constants.cs
+++ ReversePassword/Constants.cs
@@ -5,7 +5,7 @@ namespace ReversePassword
     internal static class Constants
     {
         // The CLSID GUID is also hardcoded in install.reg
-        public const string CredentialProvider_CLSID = "00006d50-0000-0000-b090-00006b0b0000";
+        public const string CredentialProvider_CLSID = "ACA40B06-9A9A-4B7B-A92C-F97FED8403B6";
 
         public const uint CREDENTIAL_PROVIDER_NO_DEFAULT = unchecked((uint)(-1));
 
