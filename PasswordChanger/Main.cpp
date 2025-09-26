a6d07a5c8382d4683c503aa8181a24f06c4f26a6
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-09-26 16:20:36 +0200

    Remove unused ChangeAccountPasswordW impl. that is not needed and doesn't work.

--- PasswordChanger/Main.cpp
+++ PasswordChanger/Main.cpp
@@ -1,14 +1,8 @@
-#define SECURITY_WIN32
-
 #include <Windows.h>
 #include <lm.h>
-#include <lmaccess.h>
-#include <ntsecapi.h>
-#include <sspi.h>
 #include <iostream>
 #include <string>
 
-#pragma comment(lib, "Secur32.lib") // for ChangeAccountPasswordW
 #pragma comment(lib, "netapi32.lib") // for NetUserChangePassword
 
 
@@ -28,35 +22,6 @@ int wmain(int argc, wchar_t* argv[])
 
     wprintf(L"Changing password for user %s...\n", username.c_str());
 
-#if 0
-    std::wstring packageName = L"Negotiate"; // "Kerberos", "Negotiate", or "NTLM".
-    BOOLEAN impersonating = false;
-
-    SecBufferDesc output{};
-    SecBuffer response{};
-    DOMAIN_PASSWORD_INFORMATION dpi{};
-    {
-        // output must contain a single SECBUFFER_CHANGE_PASS_RESPONSE buffer
-        response.BufferType = SECBUFFER_CHANGE_PASS_RESPONSE;
-        response.cbBuffer = sizeof(dpi);
-        response.pvBuffer = &dpi;
-
-        output.ulVersion = SECBUFFER_VERSION;
-        output.cBuffers = 1; // one buffer
-        output.pBuffers = &response;
-    }
-
-    SECURITY_STATUS res = ChangeAccountPasswordW(packageName.data(), nullptr, username.data(), oldPwd.data(), newPwd.data(), impersonating, 0, &output);
-    if (res != SEC_E_OK) {
-        if (res == SEC_E_SECPKG_NOT_FOUND)
-            wprintf(L"ERROR: Security package not found.\n");
-        else if (res == SEC_E_INVALID_TOKEN)
-            wprintf(L"ERROR: Invalid token.\n");
-        else
-            wprintf(L"ERROR: Password change failed with err 0x%x\n", res);
-        return -2;
-    }
-#else
     NET_API_STATUS res = NetUserChangePassword(nullptr, username.c_str(), oldPwd.c_str(), newPwd.c_str());
     if (res != NERR_Success) {
         if (res == ERROR_ACCESS_DENIED)
@@ -71,7 +36,6 @@ int wmain(int argc, wchar_t* argv[])
             wprintf(L"ERROR: Password change failed with err %u\n", res);
         return -2;
     }
-#endif
 
     wprintf(L"SUCCESS: Password changed\n");
     return 0;
