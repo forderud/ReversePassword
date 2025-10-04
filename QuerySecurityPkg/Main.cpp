ada9c6638d6191d7d2cfd3f3447fea0f4cf91c93
Fredrik Orderud <forderud@gmail.com>
2025-10-04 22:24:57 +0200

    .

--- QuerySecurityPkg/Main.cpp
+++ QuerySecurityPkg/Main.cpp
@@ -115,7 +115,7 @@ ULONG GetAuthPackage(LsaHandle& lsa, const char* name) {
     return authPkg;
 }
 
-bool LsaLogonTest(LsaHandle& lsa, ULONG authPkg, std::wstring& username, std::wstring& password) {
+bool LsaLogonUserTest(LsaHandle& lsa, ULONG authPkg, std::wstring& username, std::wstring& password) {
     const char ORIGIN[] = "QuerySecurityPkg";
     LSA_STRING origin {
         .Length = (USHORT)strlen(ORIGIN),
@@ -123,9 +123,46 @@ bool LsaLogonTest(LsaHandle& lsa, ULONG authPkg, std::wstring& username, std::ws
         .Buffer = (char*)ORIGIN,
     };
 
-    std::vector<BYTE> authInfo;
+    std::vector<BYTE> authInfo; // MSV1_0_INTERACTIVE_LOGON
     {
-        // TODO: Populate with username & password
+        std::wstring domain = L"";
+
+        // field sizes [bytes]
+        auto domainSize = (USHORT)(2 * domain.size());
+        auto usernameSize = (USHORT)(2 * username.size());
+        auto passwordSize = (USHORT)(2 * password.size());
+
+        // populate packed MSV1_0_INTERACTIVE_LOGON struct with domain, username & password at the end
+        authInfo.resize(sizeof(MSV1_0_INTERACTIVE_LOGON) + domainSize + usernameSize + passwordSize);
+        auto* logon = (MSV1_0_INTERACTIVE_LOGON*)authInfo.data();
+        logon->MessageType = MsV1_0InteractiveLogon;
+        
+        logon->LogonDomainName = {
+            .Length = domainSize,
+            .MaximumLength = domainSize,
+            .Buffer = (wchar_t*)sizeof(MSV1_0_INTERACTIVE_LOGON), // relative address
+        };
+
+        logon->UserName = {
+            .Length = usernameSize,
+            .MaximumLength = usernameSize,
+            .Buffer = (wchar_t*)(sizeof(MSV1_0_INTERACTIVE_LOGON) + domainSize), // relative address
+        };
+
+        logon->Password = {
+            .Length = passwordSize,
+            .MaximumLength = passwordSize,
+            .Buffer = (wchar_t*)(sizeof(MSV1_0_INTERACTIVE_LOGON) + domainSize + usernameSize), // relative address
+        };
+
+        BYTE* domainStart = authInfo.data() + (size_t)logon->LogonDomainName.Buffer;
+        memcpy(domainStart, domain.data(), domainSize);
+
+        BYTE* usernameStart = authInfo.data() + (size_t)logon->UserName.Buffer;
+        memcpy(usernameStart, username.data(), usernameSize);
+
+        BYTE* passwordStart = authInfo.data() + (size_t)logon->Password.Buffer;
+        memcpy(passwordStart, password.data(), passwordSize);
     }
 
     TOKEN_SOURCE sourceContext{};
