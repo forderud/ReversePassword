816aa87d304025aa17b6c42f17fa99679ab094d9
Fredrik Orderud <forderud@gmail.com>
2025-10-06 22:56:34 +0200

    .

--- NoPasswordAuthPkg/Install_NoPasswordAuthPkg.bat
+++ NoPasswordAuthPkg/Install_NoPasswordAuthPkg.bat
@@ -5,7 +5,7 @@ echo Installing authentication package DLL...
 
 copy NoPasswordAuthPkg.dll %SYSTEMROOT%\System32
 
-
+:: TODO: Update script to append instead of overwrite the security package list
 reg.exe add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Lsa" /v "Security Packages" /t REG_MULTI_SZ /d NoPasswordAuthPkg\0 /f
 
 pause
