3bfd9a8eba6f13abcc54cdcef1112d8af7b863c7
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-07 12:50:55 +0200

    .

--- SecurityPkgTester/Main.cpp
+++ SecurityPkgTester/Main.cpp
@@ -115,6 +115,10 @@ std::vector<BYTE> PrepareLogon_MSV1_0(std::wstring& username, std::wstring& pass
     return authInfo;
 }
 
+static bool IsEqual (LUID left, LUID right) {
+    return (left.LowPart == right.LowPart) && (left.HighPart == right.HighPart);
+}
+
 NTSTATUS LsaLogonUserInteractive(LsaHandle& lsa, const wchar_t* authPkgName, const std::vector<BYTE>& authInfo) {
     //wprintf(L"INFO: AuthenticationInformationLength: %u\n", (uint32_t)authInfo.size());
 
@@ -188,6 +192,39 @@ NTSTATUS LsaLogonUserInteractive(LsaHandle& lsa, const wchar_t* authPkgName, con
             wprintf(L"ERROR: Incorrect process token type. Need primary token.\n");
             return ERROR_PRIVILEGE_NOT_HELD;
         }
+    }
+
+    {
+        // privilege check
+        LUID INCREASE_QUOTA{};
+        BOOL ok = LookupPrivilegeValueW(nullptr, SE_INCREASE_QUOTA_NAME, &INCREASE_QUOTA);
+        assert(ok);
+        LUID ASSIGNPRIMARYTOKEN = {};
+        ok = LookupPrivilegeValueW(nullptr, SE_ASSIGNPRIMARYTOKEN_NAME, &ASSIGNPRIMARYTOKEN);
+        assert(ok);
+
+        std::vector<BYTE> privilegesBuffer(1024, (BYTE)0);
+        DWORD privilegesLength = 0;
+        ok = GetTokenInformation(token, TokenPrivileges, privilegesBuffer.data(), (DWORD)privilegesBuffer.size(), &privilegesLength);
+        assert(ok);
+        privilegesBuffer.resize(privilegesLength);
+        auto* privileges = (TOKEN_PRIVILEGES*)privilegesBuffer.data();
+
+        bool hasIncreaseQuta = false;
+        bool hasAssignPrimaryToken = false;
+        wprintf(L"Inspecting all %u token privileges...\n", privileges->PrivilegeCount);
+        for (size_t i = 0; i < privileges->PrivilegeCount; i++) {
+            if (IsEqual(privileges->Privileges[i].Luid, INCREASE_QUOTA))
+                hasIncreaseQuta = true;
+            if (IsEqual(privileges->Privileges[i].Luid, ASSIGNPRIMARYTOKEN))
+                hasAssignPrimaryToken = true;
+
+        }
+
+        if (!hasIncreaseQuta)
+            wprintf(L"WARNING: SE_INCREASE_QUOTA_NAME privilege missing\n");
+        if (!hasAssignPrimaryToken)
+            wprintf(L"WARNING: SE_ASSIGNPRIMARYTOKEN_NAME privilege missing\n");
     }
 
     // Start command prompt.
