8dac1916129093a4ed81ec2b09d44a8118bf3545
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-08 14:41:59 +0200

    simplify

--- ReversePassword/REGISTER_ReversePassword.bat
+++ ReversePassword/REGISTER_ReversePassword.bat
@@ -1,5 +1,4 @@
 :: Fix issue with "Run as Administrator" current dir
-setlocal enableextensions
 cd /d "%~dp0"
 
 regsvr32.exe /s ReversePassword.comhost.dll
--- ReversePassword/UNREGISTER_ReversePassword.bat
+++ ReversePassword/UNREGISTER_ReversePassword.bat
@@ -1,5 +1,4 @@
 :: Fix issue with "Run as Administrator" current dir
-setlocal enableextensions
 cd /d "%~dp0"
 
 regsvr32.exe /u /s ReversePassword.comhost.dll
