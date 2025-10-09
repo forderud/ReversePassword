2fc4a99b205ad78793019d8ebdb5bb5f50497cd4
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-09 15:52:13 +0200

    Log HighPart before LowPart.

--- NoPasswordAuthPkg/Main.cpp
+++ NoPasswordAuthPkg/Main.cpp
@@ -165,7 +165,7 @@ NTSTATUS LsaApLogonUser (
             return status;
         }
 
-        LogMessage("  LogonId: Low=0x%x , High=0x%x", LogonId->LowPart, LogonId->HighPart);
+        LogMessage("  LogonId: High=0x%x , Low=0x%x", LogonId->HighPart, LogonId->LowPart);
     }
 
     *SubStatus = STATUS_SUCCESS; // reason for error
@@ -214,7 +214,7 @@ NTSTATUS LsaApLogonUser (
 
 void LsaApLogonTerminated(_In_ LUID* LogonId) {
     LogMessage("LsaApLogonTerminated");
-    LogMessage("  LogonId: Low=0x%x , High=0x%x", LogonId->LowPart, LogonId->HighPart);
+    LogMessage("  LogonId: High=0x%x , Low=0x%x", LogonId->HighPart, LogonId->LowPart);
     LogMessage("  return");
 }
 
