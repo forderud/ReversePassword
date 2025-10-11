8631533ff62da42795a68474c702089ad8d70424
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-11 11:33:29 +0200

    Extract token checks to a new CheckTokenPrivileges function.

--- AuthPkgTester/Main.cpp
+++ AuthPkgTester/Main.cpp
@@ -12,6 +12,7 @@
 #include <tuple>
 #include <vector>
 #include "PrintInfo.hpp"
+#include "TokenSupport.hpp"
 
 #pragma comment(lib, "Secur32.lib")
 #pragma comment(lib, "Userenv.lib")
@@ -117,64 +118,13 @@ std::vector<BYTE> PrepareLogon_MSV1_0(std::wstring& username, std::wstring& pass
     return authInfo;
 }
 
-bool IsEqual (LUID left, LUID right) {
-    return (left.LowPart == right.LowPart) && (left.HighPart == right.HighPart);
-}
 
 NTSTATUS CreateCmdProcessWithTokenW(HANDLE token, const std::wstring& username, const std::wstring& password) {
-    {
-        // verify that "token" type is TokenPrimary
-        TOKEN_TYPE tokenType = {};
-        DWORD tokenLen = 0;
-        if (!GetTokenInformation(token, TokenType, &tokenType, sizeof(tokenType), &tokenLen))
-            abort();
-        if (tokenType != TokenPrimary) {
-            wprintf(L"ERROR: Incorrect process token type. Need primary token.\n");
-            return ERROR_PRIVILEGE_NOT_HELD;
-        }
-    }
+    if (!VerifyThatTokenIsPrimary(token))
+        abort();
 
-    bool hasIncreaseQuta = false;
-    bool hasAssignPrimaryToken = false;
-    bool hasImpersonateName = false;
-    {
-        // current process privilege check
-        LUID INCREASE_QUOTA{};
-        BOOL ok = LookupPrivilegeValueW(nullptr, SE_INCREASE_QUOTA_NAME, &INCREASE_QUOTA);
-        assert(ok);
-        LUID ASSIGNPRIMARYTOKEN = {};
-        ok = LookupPrivilegeValueW(nullptr, SE_ASSIGNPRIMARYTOKEN_NAME, &ASSIGNPRIMARYTOKEN);
-        assert(ok);
-        LUID IMPERSONATE_NAME = {};
-        ok = LookupPrivilegeValueW(nullptr, SE_IMPERSONATE_NAME, &IMPERSONATE_NAME);
-        assert(ok);
-
-        std::vector<BYTE> privilegesBuffer(1024, (BYTE)0);
-        DWORD privilegesLength = 0;
-        ok = GetTokenInformation(GetCurrentProcessToken(), TokenPrivileges, privilegesBuffer.data(), (DWORD)privilegesBuffer.size(), &privilegesLength);
-        assert(ok);
-        privilegesBuffer.resize(privilegesLength);
-        auto* privileges = (TOKEN_PRIVILEGES*)privilegesBuffer.data();
-
-        wprintf(L"Inspecting %u token privileges...\n", privileges->PrivilegeCount);
-        for (size_t i = 0; i < privileges->PrivilegeCount; i++) {
-            if (IsEqual(privileges->Privileges[i].Luid, INCREASE_QUOTA))
-                hasIncreaseQuta = true;
-            if (IsEqual(privileges->Privileges[i].Luid, ASSIGNPRIMARYTOKEN))
-                hasAssignPrimaryToken = true;
-            if (IsEqual(privileges->Privileges[i].Luid, IMPERSONATE_NAME))
-                hasImpersonateName = true;
-        }
-
-#if 0
-        if (!hasIncreaseQuta)
-            wprintf(L"WARNING: SE_INCREASE_QUOTA_NAME privilege missing\n");
-        if (!hasAssignPrimaryToken)
-            wprintf(L"WARNING: SE_ASSIGNPRIMARYTOKEN_NAME privilege missing\n");
-#endif
-        if (!hasImpersonateName)
-            wprintf(L"WARNING: SE_IMPERSONATE_NAME privilege missing\n");
-    }
+    // current process privilege check
+    CheckTokenPrivileges(GetCurrentProcessToken());
 
     {
 #if 0
---
+++ AuthPkgTester/TokenSupport.hpp
@@ -0,0 +1,70 @@
+#pragma once
+#include <Windows.h>
+#include <stdio.h>
+
+
+bool IsEqual(LUID left, LUID right) {
+    return (left.LowPart == right.LowPart) && (left.HighPart == right.HighPart);
+}
+
+bool VerifyThatTokenIsPrimary(HANDLE token) {
+    {
+        // verify that "token" type is TokenPrimary
+        TOKEN_TYPE tokenType = {};
+        DWORD tokenLen = 0;
+        if (!GetTokenInformation(token, TokenType, &tokenType, sizeof(tokenType), &tokenLen))
+            abort();
+        if (tokenType != TokenPrimary) {
+            wprintf(L"ERROR: Incorrect process token type. Need primary token.\n");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+bool CheckTokenPrivileges(HANDLE token) {
+    bool hasIncreaseQuta = false;
+    bool hasAssignPrimaryToken = false;
+    bool hasImpersonateName = false;
+    {
+        LUID INCREASE_QUOTA{};
+        BOOL ok = LookupPrivilegeValueW(nullptr, SE_INCREASE_QUOTA_NAME, &INCREASE_QUOTA);
+        assert(ok);
+        LUID ASSIGNPRIMARYTOKEN = {};
+        ok = LookupPrivilegeValueW(nullptr, SE_ASSIGNPRIMARYTOKEN_NAME, &ASSIGNPRIMARYTOKEN);
+        assert(ok);
+        LUID IMPERSONATE_NAME = {};
+        ok = LookupPrivilegeValueW(nullptr, SE_IMPERSONATE_NAME, &IMPERSONATE_NAME);
+        assert(ok);
+
+        std::vector<BYTE> privilegesBuffer(1024, (BYTE)0);
+        DWORD privilegesLength = 0;
+        ok = GetTokenInformation(token, TokenPrivileges, privilegesBuffer.data(), (DWORD)privilegesBuffer.size(), &privilegesLength);
+        assert(ok);
+        privilegesBuffer.resize(privilegesLength);
+        auto* privileges = (TOKEN_PRIVILEGES*)privilegesBuffer.data();
+
+        wprintf(L"Inspecting %u token privileges...\n", privileges->PrivilegeCount);
+        for (size_t i = 0; i < privileges->PrivilegeCount; i++) {
+            if (IsEqual(privileges->Privileges[i].Luid, INCREASE_QUOTA))
+                hasIncreaseQuta = true;
+            if (IsEqual(privileges->Privileges[i].Luid, ASSIGNPRIMARYTOKEN))
+                hasAssignPrimaryToken = true;
+            if (IsEqual(privileges->Privileges[i].Luid, IMPERSONATE_NAME))
+                hasImpersonateName = true;
+        }
+
+#if 0
+        if (!hasIncreaseQuta)
+            wprintf(L"WARNING: SE_INCREASE_QUOTA_NAME privilege missing\n");
+        if (!hasAssignPrimaryToken)
+            wprintf(L"WARNING: SE_ASSIGNPRIMARYTOKEN_NAME privilege missing\n");
+#endif
+        if (!hasImpersonateName)
+            wprintf(L"WARNING: SE_IMPERSONATE_NAME privilege missing\n");
+    }
+
+    return true;
+}
