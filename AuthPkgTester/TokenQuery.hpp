864cda2392341592d1a61f641fd05527ddbe7200
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-12 13:43:23 +0200

    .

--- AuthPkgTester/TokenQuery.hpp
+++ AuthPkgTester/TokenQuery.hpp
@@ -13,9 +13,8 @@ const std::wstring ToString(DWORD err) {
     case ERROR_TOKEN_ALREADY_IN_USE: return L"ERROR_TOKEN_ALREADY_IN_USE";
     case ERROR_INVALID_SECURITY_DESCR: return L"ERROR_INVALID_SECURITY_DESCR";
     case ERROR_LOGON_TYPE_NOT_GRANTED: return L"ERROR_LOGON_TYPE_NOT_GRANTED";
+    default: return L"error " + std::to_wstring(err);
     }
-
-    return L"error " + std::to_wstring(err);
 }
 
 enum class PrivilegeState {
