79dbb007aca3430331119ca4eb69adde83c7bbe3
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-10-15 16:46:54 +0200

    switch to calloc/free for allocation.

--- AuthPkgTester/TokenUtils.hpp
+++ AuthPkgTester/TokenUtils.hpp
@@ -304,8 +304,7 @@ BOOL GetLogonSID (HANDLE hToken, PSID* ppsid) {
         if (GetLastError() != ERROR_INSUFFICIENT_BUFFER)
             goto Cleanup;
 
-        ptg = (PTOKEN_GROUPS)HeapAlloc(GetProcessHeap(),
-            HEAP_ZERO_MEMORY, dwLength);
+        ptg = (PTOKEN_GROUPS)calloc(1, dwLength);
 
         if (ptg == NULL)
             goto Cleanup;
@@ -327,13 +326,12 @@ BOOL GetLogonSID (HANDLE hToken, PSID* ppsid) {
         if ((ptg->Groups[dwIndex].Attributes & SE_GROUP_LOGON_ID) == SE_GROUP_LOGON_ID) {
             // Found the logon SID; make a copy of it.
             dwLength = GetLengthSid(ptg->Groups[dwIndex].Sid);
-            *ppsid = (PSID)HeapAlloc(GetProcessHeap(),
-                HEAP_ZERO_MEMORY, dwLength);
+            *ppsid = (PSID)calloc(1, dwLength);
             if (*ppsid == NULL)
                 goto Cleanup;
             if (!CopySid(dwLength, *ppsid, ptg->Groups[dwIndex].Sid))
             {
-                HeapFree(GetProcessHeap(), 0, (LPVOID)*ppsid);
+                free(*ppsid);
                 goto Cleanup;
             }
             break;
@@ -344,7 +342,7 @@ BOOL GetLogonSID (HANDLE hToken, PSID* ppsid) {
 
 Cleanup:
     if (ptg)
-        HeapFree(GetProcessHeap(), 0, (LPVOID)ptg);
+        free(ptg);
 
     return bSuccess;
 }
