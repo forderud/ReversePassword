c5d51b75e5e5f93c98018d2eb72605dda86988cb
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-06-13 11:33:42 +0200

    .

--- WebCredMgr/Main.cpp
+++ WebCredMgr/Main.cpp
@@ -3,29 +3,28 @@
 #include <iostream>
 #include <cassert>
 
-//#pragma comment(lib, "Advapi32.lib") // CredReadW
-
-
-bool StoreCredential(const std::wstring& username, const std::wstring& password) {
+/** Store credential in Windows Credential Manager. */
+bool StoreCredential(const std::wstring& username, const std::wstring& secret) {
     CREDENTIALW cred{};
     cred.Flags = 0;
     cred.Type = CRED_TYPE_GENERIC;
     cred.Persist = CRED_PERSIST_LOCAL_MACHINE; // disable roaming
     cred.TargetName = const_cast<WCHAR*>(username.c_str());
-    cred.CredentialBlob = reinterpret_cast<BYTE*>(const_cast<WCHAR*>(password.data()));
-    cred.CredentialBlobSize = (DWORD)password.length()*sizeof(WCHAR);
+    cred.CredentialBlob = reinterpret_cast<BYTE*>(const_cast<WCHAR*>(secret.data()));
+    cred.CredentialBlobSize = (DWORD)secret.length()*sizeof(WCHAR);
 
     BOOL ok = CredWriteW(&cred, 0);
     return ok;
 }
 
-bool LoadCredential(const std::wstring& username, /*out*/std::wstring& password) {
+/** Load credential from Windows Credential Manager. */
+bool LoadCredential(const std::wstring& username, /*out*/std::wstring& secret) {
     CREDENTIALW* cred = nullptr;
     BOOL ok = CredReadW(username.c_str(), CRED_TYPE_GENERIC, 0, &cred);
     if (!ok)
         return false;
     
-    password.assign(reinterpret_cast<WCHAR*>(cred->CredentialBlob), cred->CredentialBlobSize / sizeof(WCHAR));
+    secret.assign(reinterpret_cast<WCHAR*>(cred->CredentialBlob), cred->CredentialBlobSize/sizeof(WCHAR));
     CredFree(cred);
     return true;
 }
@@ -35,7 +34,10 @@ int main() {
     const WCHAR username[] = L"TestUser";
 
     bool ok = StoreCredential(username, L"Password123");
-    assert(ok);
+    if (!ok) {
+        std::wcerr << L"Failed to store credential. Error code: " << GetLastError() << std::endl;
+        return 1;
+    }
 
     std::wstring password;
     ok = LoadCredential(username, password);
