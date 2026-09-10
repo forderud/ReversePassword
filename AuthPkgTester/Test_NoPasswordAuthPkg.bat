7dff52581d3759b634e0c75473464e609b2aa3c6
Fredrik Orderud <forderud@gmail.com>
2026-09-10 21:01:08 +0200

    .

--- AuthPkgTester/Test_NoPasswordAuthPkg.bat
+++ AuthPkgTester/Test_NoPasswordAuthPkg.bat
@@ -1,5 +1,8 @@
+@echo off
 :: Fix issue with "Run as Administrator" current dir
 cd /d "%~dp0"
+
+echo NOTICE: Script must run from an admin command-prompt.
 
 AuthPkgTester.exe NoPasswordAuthPkg TestUser IncorrectPassword
 
