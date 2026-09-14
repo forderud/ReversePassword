baf1c70e26a01df75c8c7cbaff0816bf249c1fa5
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2026-09-14 20:18:21 +0200

    Delete test script of limited use.

--- AuthPkgTester/Test_NoPasswordAuthPkg.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Fix issue with "Run as Administrator" current dir
-cd /d "%~dp0"
-
-echo NOTICE: Script must run from an admin command-prompt.
-
-AuthPkgTester.exe NoPasswordAuthPkg TestUser IncorrectPassword
-
-pause
